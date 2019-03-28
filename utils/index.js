43da9d57daa325c6401cbb94e75c25af451fc342
Slyke (Desktop) <steven.lawler777@gmail.com>
2019-03-27 20:51:27 -0400

    Added assembly version to converter

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,14 @@
-let opCommand = "mov";
+let opCommand = "0x06";
 let processArgs = process.argv;
 let charComment = false;
 let stringTerminatorChar = true;
+let assemlyLineLength = 20; // Newlines every X commands, to avoid serial overlfow.
+let assemblyOutput = false;
 let argCount = 0;
+let padding = 2;
+
+let assemblyOut = "";
+let commandCounter = 0;
 
 for (let i = 2; i < processArgs.length; i++) {
   if (processArgs[i] === "-op") {
@@ -16,6 +22,15 @@ for (let i = 2; i < processArgs.length; i++) {
   } else if (processArgs[i] === "-nt") {
     stringTerminatorChar = false;
     argCount++;
+  } else if (processArgs[i] === "-asm") {
+    assemblyOutput = true;
+    argCount++;
+  } else if (processArgs[i] === "-p") {
+    if (processArgs[i + 1] !== undefined) {
+      padding = parseInt(processArgs[i + 1]);
+    }
+    
+    argCount += 2;
   }
 }
 
@@ -24,24 +39,32 @@ if (processArgs.length < argCount + 3) {
   process.exit(1);
 }
 
+function pad(str, size, withChar = "0", atEnd = false) {
+  var s = str + "";
+  if (atEnd) {
+    while (s.length < size) {
+      s = s + withChar;
+    }
+  } else {
+    while (s.length < size) {
+      s = withChar + s;
+    }
+  }
+  return s;
+}
+
 function convertToHex(inputChar, charIndex) {
   if (inputChar === 0x00) {
     if (charComment) {
-      return `0x00 // Input (${charIndex}): \\0`;
+      return `0x${pad('00', padding > -1 ? padding : 2)} // Input (${charIndex}): \\0`;
     }
-    return "0x00";
+    return `0x${pad('00', padding > -1 ? padding : 2)}`;
   }
 
   if (charComment) {
-    return `0x${inputChar.charCodeAt(0).toString(16)} // Input (${charIndex}): ${inputChar[0]}`;
+    return `0x${pad(inputChar.charCodeAt(0).toString(16), padding > -1 ? padding : 2)} // Input (${charIndex}): ${inputChar[0]}`;
   }
-  return "0x" + inputChar.charCodeAt(0).toString(16);
-}
-
-function pad(str, size, withChar = "0") {
-  var s = str + "";
-  while (s.length < size) s = withChar + s;
-  return s;
+  return "0x" + pad(inputChar.charCodeAt(0).toString(16), padding > -1 ? padding : 2);
 }
 
 for (let i = argCount + 2; i < processArgs.length; i += 2) {
@@ -67,12 +90,28 @@ for (let i = argCount + 2; i < processArgs.length; i += 2) {
 
   let processString = processArgs[i + 1];
 
-  for (let j = 0; j < processString.length; j++) {
-    console.log(`${opCommand} 0x${pad((startingAddress + j).toString(16), 4)} ${convertToHex(processString[j], j + 1)}`);
+  if (assemblyOutput) {
+    for (let j = 0; j < processString.length; j++) {
+      assemblyOut += `${opCommand} 0x${pad((startingAddress + j).toString(16), padding > -1 ? padding : 4)} ${convertToHex(processString[j], j + 1)} `;
+      commandCounter++;
+      if (assemlyLineLength > 0 && commandCounter > assemlyLineLength) {
+        console.log(assemblyOut);
+        commandCounter = 0;
+        assemblyOut = "";
+      }
+    }
+  } else {
+    for (let j = 0; j < processString.length; j++) {
+      console.log(`${opCommand} 0x${pad((startingAddress + j).toString(16), padding > -1 ? padding : 4)} ${convertToHex(processString[j], j + 1)}`);
+    }
+    if (stringTerminatorChar) {
+      console.log(`${opCommand} 0x${pad((processString.length).toString(16), padding > -1 ? padding : 4)} ${convertToHex(0x00, processString.length)}`);
+    }
+    console.log("");
   }
-  if (stringTerminatorChar) {
-    console.log(`${opCommand} 0x${pad((processString.length).toString(16), 4)} ${convertToHex(0x00, processString.length)}`);
-  }
-  console.log("");
+}
+
+if (commandCounter > 0 && assemblyOut.length > 3) {
+  console.log(assemblyOut);
 }
 
